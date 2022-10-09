db35dd9e35f3d1b51bf18fab17a78244fbd334da
Halit Burak Yeşildal <halit_burak@outlook.com>
2022-10-09 16:14:29 +0300

    user model created

---
+++ upsoon/user/src/main/java/com/upsoon/user/model/User.java
@@ -0,0 +1,14 @@
+package com.upsoon.user.model;
+
+@Entity
+public class User {
+
+
+    private String name;
+    private String lastName;
+    private String middleName;
+    private String identificationNumber;
+    private String phoneNumber;
+    
+
+}
