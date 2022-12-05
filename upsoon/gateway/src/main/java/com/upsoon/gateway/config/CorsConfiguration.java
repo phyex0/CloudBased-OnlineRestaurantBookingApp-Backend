19aac1872811bee23108be498d42a01851539660
Halit Burak Yeşildal <halit_burak@outlook.com>
2022-12-05 10:17:13 +0300

    fix

--- upsoon/gateway/src/main/java/com/upsoon/gateway/config/CorsConfiguration.java
+++ upsoon/gateway/src/main/java/com/upsoon/gateway/config/CorsConfiguration.java
@@ -1,16 +1,10 @@
 package com.upsoon.gateway.config;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 /**
@@ -19,27 +13,17 @@ import java.io.IOException;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfiguration implements Filter {
+public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void addCorsMappings(CorsRegistry registry) {
 
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        HttpServletRequest request= (HttpServletRequest) servletRequest;
-
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-        filterChain.doFilter(servletRequest, servletResponse);
-    }
-
-    @Override
-    public void destroy() {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowCredentials(true).maxAge(3600);
 
     }
 }
