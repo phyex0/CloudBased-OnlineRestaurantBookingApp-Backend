ff6f37cb0c4189a979702246be7126c2ebf32369
Halit Burak Yeşildal <halit_burak@outlook.com>
2022-12-12 18:49:05 +0300

    fix

--- upspoon/organization/src/main/java/com/upspoon/organization/controller/OrganizationController.java
+++ upspoon/organization/src/main/java/com/upspoon/organization/controller/OrganizationController.java
@@ -6,8 +6,6 @@ import com.upspoon.common.dto.Organization.NewOrganizationDTO;
 import com.upspoon.common.dto.Organization.UpdateOrganizationDTO;
 import com.upspoon.common.web.CustomPage;
 import com.upspoon.organization.service.OrganizationService;
-import io.swagger.annotations.Api;
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +18,6 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/organization")
-@Api(value = "Organization Controller")
 public class OrganizationController {
 
     private final OrganizationService organizationSerice;
@@ -31,45 +28,38 @@ public class OrganizationController {
 
 
     @PostMapping
-    @Operation(summary = "Creates new organization by taking OrganizationDTO and RestaurantUserDTO")
     public ResponseEntity<NewOrganizationCreateDTO> createOrganization(@RequestBody NewOrganizationCreateDTO newOrganizationCreateDTO) {
         return organizationSerice.createOrganization(newOrganizationCreateDTO);
     }
 
     @PostMapping("/create-restaurant")
-    @Operation(summary = "Creates a new organization and links to given organization")
     public ResponseEntity<NewOrganizationDTO> createRestaurant(@RequestParam(value = "organizationId") UUID organizationId, @RequestBody NewOrganizationDTO newOrganizationDTO) {
         return organizationSerice.createRestaurant(organizationId, newOrganizationDTO);
     }
 
 
     @GetMapping
-    @Operation(summary = "Returns all of the organizations and restaurant for related restaurantUser")
     public ResponseEntity<CustomPage<NewOrganizationDTO>> getAllOrganizations(@RequestParam(value = "restaurantUserId") UUID restaurantUserId, Pageable pageable) {
         return organizationSerice.getAllOrganizations(restaurantUserId, pageable);
     }
 
 
     @DeleteMapping
-    @Operation(summary = "Deletes the given restaurant")
     public ResponseEntity<Void> deleteRestaurant(@RequestParam(value = "restaurantId") UUID restaurantId) {
         return organizationSerice.deleteRestaurant(restaurantId);
     }
 
     @GetMapping("/link-user-to-given-restaurant")
-    @Operation(summary = "Given user is linked to given restaurant")
     public ResponseEntity<Void> linkUserToGivenRestaurant(@RequestParam(value = "restaurantId") UUID restaurantId, @RequestParam(value = "userId") UUID userId) {
         return organizationSerice.linkUserToGivenRestaurant(restaurantId, userId);
     }
 
     @DeleteMapping("/unlink-user-from-given-restaurant")
-    @Operation(summary = "Given user is removed from given restaurant")
     public ResponseEntity<Void> unlinkGivenUserFromGivenRestaurant(@RequestParam(value = "userId") UUID userId, @RequestParam(value = "restaurantId") UUID restaurantId) {
         return organizationSerice.unlinkGivenUserFromGivenRestaurant(userId, restaurantId);
     }
 
     @PutMapping
-    @Operation(summary = "Updates given restaurant")
     public ResponseEntity<UpdateOrganizationDTO> updateRestaurant(@RequestParam(value = "restaurantId") UUID restaurantId, @RequestBody UpdateOrganizationDTO organizationDTO) {
         return organizationSerice.updateRestaurant(restaurantId, organizationDTO);
     }
--- upspoon/organization/src/main/java/com/upspoon/organization/controller/RestaurantUserController.java
+++ upspoon/organization/src/main/java/com/upspoon/organization/controller/RestaurantUserController.java
@@ -5,7 +5,6 @@ import com.upspoon.common.dto.Organization.NewRestaurantUserDTO;
 import com.upspoon.common.dto.Organization.UpdateRestaurantUserDTO;
 import com.upspoon.common.web.CustomPage;
 import com.upspoon.organization.service.RestaurantUserService;
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,25 +26,21 @@ public class RestaurantUserController {
     }
 
     @PostMapping
-    @Operation(summary = "Creates new user and links to given restaurant")
     public ResponseEntity<NewRestaurantUserDTO> addNewUserToRestaurant(@RequestParam(value = "restaurantId") UUID restaurantId, @RequestBody NewRestaurantUserDTO newRestaurantUserDTO) {
         return restaurantUserService.addNewUserToRestaurant(restaurantId, newRestaurantUserDTO);
     }
 
     @GetMapping
-    @Operation(summary = "Returns all of the restaurant user related to given organization")
     public ResponseEntity<CustomPage<NewRestaurantUserDTO>> getAllUsers(@RequestParam(value = "organizationId") UUID organizationId, Pageable pageable) {
         return restaurantUserService.getAllUsers(organizationId, pageable);
     }
 
     @DeleteMapping
-    @Operation(summary = "Deletes the given user")
     public ResponseEntity<Void> deleteUser(@RequestParam(value = "userId") UUID userId) {
         return restaurantUserService.deleteUser(userId);
     }
 
     @PutMapping
-    @Operation(summary = "Updates the given user")
     public ResponseEntity<UpdateRestaurantUserDTO> updateUser(@RequestParam(value = "userId") UUID userId, @RequestBody UpdateRestaurantUserDTO restaurantUserDTO) {
         return restaurantUserService.updateUser(userId, restaurantUserDTO);
     }
