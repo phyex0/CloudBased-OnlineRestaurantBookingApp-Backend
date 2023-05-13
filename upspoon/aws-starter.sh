0752ba6675da95a0065a9bfca6c68c81d644ccb4
Halit Burak Yeşildal <halit_burak@outlook.com>
2023-05-13 22:53:45 +0300

    Amazon ECR EKS

--- upspoon/aws-starter.sh
+++ upspoon/aws-starter.sh
@@ -7,8 +7,12 @@
 #AWS-Ingress: https://www.fullstackbook.com/devops/tutorials/how-to-set-up-aws-load-balancer-controller/
 
 
+#To enable AWS CLI create a User and attach policy. After that create "Access Keys". When you have public and private keys:
+#aws confiugre public_key, private_key
+
+
 #to built eks cluster
-eksctl create cluster --name upspoon-cluster --nodegroup-name linux-nodes --node-type t2.micro --nodes 2
+eksctl create cluster --name upspoon-cluster --nodegroup-name linux-nodes --node-type t2.micro --nodes 5
 
 #to delete eks cluster
 eksctl delete cluster --name upspoon-cluster
@@ -16,16 +20,15 @@ eksctl delete cluster --name upspoon-cluster
 #to get userId and arn
 aws sts get-caller-identity
 
-# run the script:
+
+# run the script to add Ingress
 oidc_id=$(aws eks describe-cluster --name upspoon-cluster --query "cluster.identity.oidc.issuer" --output text | cut -d '/' -f 5)
 
 aws iam list-open-id-connect-providers | grep $oidc_id
 
 eksctl utils associate-iam-oidc-provider --cluster upspoon-cluster --approve
-
-
 ###
-curl -o iam_policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.4/docs/install/iam_policy.json
+curl -k -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.7/docs/install/iam_policy.json
 
 aws iam create-policy \
     --policy-name AWSLoadBalancerControllerIAMPolicy \
@@ -35,25 +38,19 @@ eksctl create iamserviceaccount \
   --cluster=upspoon-cluster \
   --namespace=kube-system \
   --name=aws-load-balancer-controller \
-  --role-name "AmazonEKSLoadBalancerControllerRole" \
+  --role-name AmazonEKSLoadBalancerControllerRole \
   --attach-policy-arn=arn:aws:iam::980605516176:policy/AWSLoadBalancerControllerIAMPolicy \
   --approve
 
-kubectl apply \
-    --validate=false \
-    -f https://github.com/jetstack/cert-manager/releases/download/v1.5.4/cert-manager.yaml
+helm repo add eks https://aws.github.io/eks-charts
 
-curl -Lo v2_4_4_full.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.4.4/v2_4_4_full.yaml
+helm repo update
 
-sed -i.bak -e '480,488d' ./v2_4_4_full.yaml
-
-sed -i.bak -e 's|your-cluster-name|upspoon-cluster|' ./v2_4_4_full.yaml
-
-kubectl apply -f v2_4_4_full.yaml
-
-curl -Lo v2_4_4_ingclass.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.4.4/v2_4_4_ingclass.yaml
-
-kubectl apply -f v2_4_4_ingclass.yaml
+helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
+  -n kube-system \
+  --set clusterName=upspoon-cluster \
+  --set serviceAccount.create=false \
+  --set serviceAccount.name=aws-load-balancer-controller
 
 #following command verifies deployment runs
 kubectl get deployment -n kube-system aws-load-balancer-controller
